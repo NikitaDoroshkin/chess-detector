337ed45fe8722311ab7faa728eee24b7fb0fe73e
mdaroshkin <mdaroshkin@aligntech.com>
2023-04-06 14:23:41 +0200

    Configure Django paths for request handlers

--- chessboard/chessboard/urls.py
+++ chessboard/chessboard/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chessapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('recognize_chessboard/', views.recognize_chessboard, name='recognize_chessboard'),
 ]
